2011d09b48fd01accf139183be3178c057bf84ba
Andres Arana <and2arana@gmail.com>
2019-02-12 19:38:44 -0300

    Adds extra features to tracks endpoint

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -1,36 +1,123 @@
+const constant = require('lodash/fp/constant');
+const filter = require('lodash/fp/filter');
+const flatMap = require('lodash/fp/flatMap');
+const flow = require('lodash/fp/flow');
+const identity = require('lodash/fp/identity');
+const isEmpty = require('lodash/fp/isEmpty');
+const map = require('lodash/fp/map');
+const reduce = require('lodash/fp/reduce');
+const reject = require('lodash/fp/reject');
+const zip = require('lodash/fp/zip');
+const keys = require('lodash/fp/keys');
 const sql = require('../services/google/sql');
+const log = require('../data/log');
+
+const additionalFeatureSettings = {
+  fishing: {
+    extraFields: 'scores',
+    updateCoordinateProperties: segment => identity,
+    buildExtraGeoJSONFeatures: segment => {
+      const coordinates = flow(
+        zip(segment.scores),
+        filter(([score, coordinate]) => score > 0.5),
+        map(([score, coordinate]) => coordinate),
+      )(segment.geojson.coordinates);
+
+      if (isEmpty(coordinates)) {
+        return {};
+      }
+
+      return {
+        type: "Feature",
+        properties: {
+          track: segment.id,
+          type: "fishing",
+        },
+        geometry: {
+          type: "MultiPoint",
+            coordinates,
+        },
+      };
+    },
+  },
+
+  course: {
+    extraFields: 'courses',
+    updateCoordinateProperties: segment => coordinateProperties => ({
+      courses: segment.courses,
+      ...coordinateProperties,
+    }),
+    buildExtraGeoJSONFeatures: constant([]),
+  },
+
+  speed: {
+    extraFields: 'speeds',
+    updateCoordinateProperties: segment => coordinateProperties => ({
+      speeds: segment.speeds,
+      ...coordinateProperties,
+    }),
+    buildExtraGeoJSONFeatures: constant([]),
+  },
+};
+
+const segmentToGeoJSONTrack = (segment, extraFeatures = []) => {
+  const geometry = segment.geojson;
+  if (geometry.coordinates.length > 1) {
+    geometry.type = 'LineString';
+  }
+
+  const reduceCoordinateProperties = (result, feature) => feature
+    .updateCoordinateProperties(segment)(result);
+
+  const coordinateProperties = reduce(reduceCoordinateProperties)({times: segment.times})(extraFeatures);
+
+  return {
+    type: "Feature",
+    properties: {
+      id: segment.id,
+      type: "track",
+      coordinateProperties,
+    },
+    geometry,
+  };
+};
+
+const queryResultsToGeoJSON = (segments, extraFeatures) => {
+  log.debug("Query results available, converting to GeoJSON");
+  const features = flow(
+    map(segment => ({
+      ...segment,
+      geojson: JSON.parse(segment.geojson),
+    })),
+    flatMap(segment => [
+      segmentToGeoJSONTrack(segment, extraFeatures),
+      ...map(feature => feature.buildExtraGeoJSONFeatures(segment))(extraFeatures),
+    ]),
+    reject(isEmpty),
+  )(segments);
+
+  return {
+    type: "FeatureCollection",
+    features,
+  };
+};
 
 module.exports = dataset => ({
-  forVessel(vesselId) {
+  forVessel(vesselId, additionalFeatures = []) {
+    log.debug(`Looking up track for vessel ${vesselId} including features ${additionalFeatures}`);
+    const extraFeatures = map(feature => additionalFeatureSettings[feature])(additionalFeatures);
+
     const query = sql
       .select(
-        "times",
-        sql.raw('ST_AsGeoJSON("seg_geography") as "geojson"')
+        'tracks.seg_id as id',
+        'times',
+        sql.raw('ST_AsGeoJSON("seg_geography") as "geojson"'),
+        ...extraFeatures.map(feature => feature.extraFields),
       )
       .from({tracks: dataset.tracksTable})
       .innerJoin({vessels: dataset.vesselsTable}, "tracks.seg_id", "vessels.seg_id")
       .where("vessels.vessel_id", vesselId);
 
-    return query.then((records) => {
-      const features = records.map((segment) => {
-        const geometry = JSON.parse(segment.geojson);
-        geometry.type = 'LineString';
-
-        return {
-          type: "Feature",
-          properties: {
-            coordinateProperties: {
-              times: segment.times,
-            },
-          },
-          geometry,
-        };
-      });
-
-      return {
-        type: "FeatureCollection",
-        features,
-      }
-    });
+    return query.then((segments) => queryResultsToGeoJSON(segments, extraFeatures));
   },
 });
--- src/routes/vessels.js
+++ src/routes/vessels.js
@@ -1,14 +1,20 @@
 const datasets = require('../data/datasets');
 const vessels = require('../data/vessels');
 const tracks = require('../data/tracks');
+const log = require('../data/log');
 
 const loadDataset = async (req, res, next) => {
   try {
     const datasetId = req.swagger.params.dataset.value;
+
+    log.debug(`Looking up dataset ${datasetId}`)
     const dataset = await datasets.get(datasetId);
     if (!dataset) {
+      log.debug("Dataset not found");
       return res.sendStatus(404);
     }
+
+    log.debug("Dataset found", dataset);
     req.dataset = dataset;
     return next();
   } catch (err) {
@@ -25,8 +31,10 @@ module.exports = (app) => {
         offset: req.swagger.params.offset.value,
       };
 
+      log.debug("Querying vessels search index");
       const results = await vessels(req.dataset).search(query);
 
+      log.debug(`Returning ${results.entries.length} / ${results.total} results`)
       return res.json(results);
     } catch(error) {
       return next(error);
@@ -37,8 +45,10 @@ module.exports = (app) => {
     try {
       const vesselId = req.swagger.params.vesselId.value;
 
+      log.debug(`Looking up vessel information for vessel ${vesselid}`);
       const result = await vessels(req.dataset).get(vesselId);
 
+      log.debug("Returning vessel information");
       return res.json(result);
     } catch(error) {
       return next(error);
@@ -50,8 +60,10 @@ module.exports = (app) => {
     try {
       const vesselId = req.swagger.params.vesselId.value;
 
-      const result = await tracks(req.dataset).forVessel(vesselId);
+      log.debug(`Looking up track for vessel ${vesselId}`);
+      const result = await tracks(req.dataset).forVessel(vesselId, req.swagger.params.features.value);
 
+      log.debug(`Returning track for vessel ${vesselId}`);
       return res.json(result);
     } catch(error) {
       return next(error);
